eb300e995d4ee9129af5c86bc9a4aeeb71a44a72
Pengu4 <abe.lancaster@students.plymouth.ac.uk>
2023-10-05 12:53:26 +0100

    changes week 2

--- Tasks/Task-101-Blinky/main.cpp
+++ Tasks/Task-101-Blinky/main.cpp
@@ -2,7 +2,8 @@
 
 // Create a DigitalOut “object” called greenLED. Pass constant LED1 as a “parameter”
 DigitalOut greenLED(LED1);
-
+DigitalOut blueLED(LED2);
+DigitalOut redLED(LED3);
 //These are "commented out" and so are not part of the program. You can uncomment them by removing the // characters
 //Your task is to make a sequence alternating between Green+Red and just Blue 
 //DigitalOut blueLED(LED2);
@@ -11,6 +12,7 @@ DigitalOut greenLED(LED1);
 //The main function - all executable C / C++ applications have a main function. This is our entry point in the software
 int main() 
 {
+    blueLED = 1;
     // ALL the repeating code is contained in a  “while loop”
     while(true) 
     { 
@@ -18,12 +20,14 @@ int main()
 
         // Turn onboard LED ON  
         greenLED = 1; 
+        redLED = 0;
 
         // Wait 0.2 second (1 million microseconds)
         wait_us(100000); 
 
         // Turn LED OFF
         greenLED = 0;
+        redLED = 1;
 
         // Wait 0.2 second
         wait_us(100000); 
